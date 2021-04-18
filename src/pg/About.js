9ab49fa3010a139de009756269d98106aab6fdc7
Alfred <castellum.a@gmail.com>
2021-04-18 20:42:51 +0200

    Moved inline styles to sass style sheet

--- src/pg/About.js
+++ src/pg/About.js
@@ -9,41 +9,12 @@ export const AboutPg = () => {
   return (
     <div className={`page ${showNav ? "" : "full"}`}>
       <h2 className="pageTitle">Who I Am</h2>
-      <main
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          paddingBottom: "4vw",
-        }}
-      >
-        <aside
-          style={{
-            width: "16vmax",
-            height: "16vmax",
-            border: "3px solid ghostwhite",
-            borderRadius: "50%",
-            margin: "2vh 1rem 1rem",
-            contain: "content",
-          }}
-        >
-          <img
-            src={meHeadshot}
-            alt="Alfred Headshot"
-            style={{
-              width: "16vmax",
-              marginLeft: "1vh",
-            }}
-          />
+      <main id="mainMain">
+        <aside id="Me">
+          <img src={meHeadshot} alt="Alfred Headshot" />
         </aside>
 
-        <main
-          id="aboutMain"
-          className={`${showNav ? "trans" : "full"}`}
-          style={{
-            width: "50vw",
-            paddingRight: "1rem",
-          }}
-        >
+        <main id="aboutMain" className={`${showNav ? "trans" : "full"}`}>
           <p style={{ marginTop: "0" }}>
             I'm currently based in The Hague, Netherlands. Originally I'm from
             So'California. I've recently switched careers from Legal Videography
@@ -59,7 +30,7 @@ export const AboutPg = () => {
             schooling, clients and personal.
           </p>
 
-          <p id={ showNav ? "endP" : "endPTrue"}>
+          <p id={showNav ? "endP" : "endPTrue"}>
             In my leisure time (although rare :p) I enjoy watching movies. I'm a
             huge Sci-Fi and Horror fan. I watch some Anime, too. I also love to
             travel and eat! Message me for any questions, comments or inquiries,
